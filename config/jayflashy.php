1d1aa436921f85ce2540dd12c203da406171e9f7
Joshua Adeyemi <jadesdevelopers@gmail.com>
2023-10-06 13:10:37 +0100

    Service provider files

---
+++ config/jayflashy.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+
+    /**
+     * Public Key From Paystack Dashboard
+     *
+     */
+    'appName' => getenv('APP_NAME'),
+
+    /**
+     * Secret Key From Paystack Dashboard
+     *
+     */
+    'appUrl' => getenv('APP_URL'),
+
+
+];
--- src/JayServiceProvider.php
+++ src/JayServiceProvider.php
@@ -2,6 +2,32 @@
 
 namespace Jayflashy\LicenseChecker;
 
-class JayServiceProvider {
+use Illuminate\Support\ServiceProvider;
+
+class JayServiceProvider  extends ServiceProvider
+{
+
+    /*
+    * Indicates if loading of the provider is deferred.
+    *
+    * @var bool
+    */
+    protected $defer = false;
+
+    public function boot()
+    {
+        $this->publishes([
+            __DIR__.'/../config/jayflashy.php' => config_path('jayflashy.php'),
+        ]);
+    }
+
+    /**
+     * Register the command.
+     */
+    public function register()
+    {
+        $this->mergeConfigFrom(__DIR__.'/../config/jayflashy.php', 'jayflashy');
+
+    }
 
 }
